457be1faefbd1e87bc59889488669a289adc5a34
Poly Bug <polybugger@gmail.com>
2016-04-12 11:27:55 +0800

    added ClassActivity

--- app/src/main/java/net/polybugger/apollot/MainActivity.java
+++ app/src/main/java/net/polybugger/apollot/MainActivity.java
@@ -2,11 +2,8 @@ package net.polybugger.apollot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
 import net.polybugger.apollot.db.ApolloDbAdapter;
-import net.polybugger.apollot.db.ClassItemContract;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +16,5 @@ public class MainActivity extends AppCompatActivity {
         ApolloDbAdapter.close();
 
         setContentView(R.layout.activity_main);
-
-        //((TextView) findViewById(R.id.text_view)).setText(ClassItemContract.SELECT_TABLE_SQL1 + ClassItemContract.SELECT_TABLE_SQL2);
-        //Log.d("SQLITE", ClassItemContract.SELECT_TABLE_SQL1 + ClassItemContract.SELECT_TABLE_SQL2);
     }
 }
