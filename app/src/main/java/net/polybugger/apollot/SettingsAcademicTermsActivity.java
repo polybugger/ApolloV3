dc882f4850e63fa585c32dd01cd82803952fb3c6
Poly Bug <polybugger@gmail.com>
2016-04-18 13:40:09 +0800

    added RecyclerView to SettingsAcademicTermsActivity and fancy elevation and background
    Woohoo!

--- app/src/main/java/net/polybugger/apollot/SettingsAcademicTermsActivity.java
+++ app/src/main/java/net/polybugger/apollot/SettingsAcademicTermsActivity.java
@@ -1,18 +1,40 @@
 package net.polybugger.apollot;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import net.polybugger.apollot.db.AcademicTermContract;
+import net.polybugger.apollot.db.ApolloDbAdapter;
+
+import java.util.ArrayList;
 
 public class SettingsAcademicTermsActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private Adapter mAdapter;
+    private LinearLayoutManager mLinearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ApolloDbAdapter.setAppContext(getApplicationContext());
 
         setContentView(R.layout.activity_settings_academic_terms);
 
@@ -20,6 +42,17 @@ public class SettingsAcademicTermsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationIcon(R.drawable.ic_close_white_24dp);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+        SQLiteDatabase db = ApolloDbAdapter.open();
+        mAdapter = new Adapter(AcademicTermContract._getEntries(db));
+        ApolloDbAdapter.close();
+        mRecyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,5 +79,57 @@ public class SettingsAcademicTermsActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+
+    public static class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+        private ArrayList<AcademicTermContract.AcademicTermEntry> mArrayList;
+
+        public Adapter(ArrayList<AcademicTermContract.AcademicTermEntry> arrayList) {
+            mArrayList = arrayList;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_settings_academic_terms, parent, false);
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            AcademicTermContract.AcademicTermEntry entry = mArrayList.get(position);
+            int color;
+            try {
+                color = Color.parseColor(entry.getColor());
+            }
+            catch(Exception e) {
+                color = Color.TRANSPARENT;
+            }
+            GradientDrawable bg = (GradientDrawable) holder.mBackgroundLayout.getBackground();
+            bg.setColor(color);
+            holder.mTextView.setText(entry.getDescription());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mArrayList.size();
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+
+            protected LinearLayout mBackgroundLayout;
+            protected LinearLayout mClickableLayout;
+            protected TextView mTextView;
+            protected ImageButton mImageButton;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                mBackgroundLayout = (LinearLayout) itemView.findViewById(R.id.background_layout);
+                mClickableLayout = (LinearLayout) itemView.findViewById(R.id.clickable_layout);
+                mTextView =  (TextView) itemView.findViewById(R.id.text_view);
+                mImageButton = (ImageButton) itemView.findViewById(R.id.image_button);
+            }
+        }
     }
 }
