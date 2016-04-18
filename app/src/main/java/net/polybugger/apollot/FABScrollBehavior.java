affa3f767f3bcae02489d5d803142a472f39591f
Poly Bug <polybugger@gmail.com>
2016-04-19 00:38:36 +0800

    fixed bug snackbar hiding FAB

--- app/src/main/java/net/polybugger/apollot/FABScrollBehavior.java
+++ app/src/main/java/net/polybugger/apollot/FABScrollBehavior.java
@@ -4,9 +4,10 @@ import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
+
+// http://androidessence.com/hide-the-floatingactionbutton-when-scrolling-a-recyclerview/
 
 public class FABScrollBehavior extends FloatingActionButton.Behavior {
 
@@ -15,18 +16,12 @@ public class FABScrollBehavior extends FloatingActionButton.Behavior {
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
-        if(dependency instanceof RecyclerView)
-            return true;
-        return false;
-    }
-
-    @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+        if(dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
             child.hide();
-        } else if (dyConsumed < 0 && child.getVisibility() == View.GONE) {
+        }
+        else if(dyConsumed < 0 && child.getVisibility() == View.GONE) {
             child.show();
         }
     }
