7e2be803e3c845d3a1e45f5a57c4b2178890bdbe
Poly Bug <polybugger@gmail.com>
2016-05-22 09:52:12 +0800

    localization changes, date time and lock/unlock class menu label

--- app/src/main/java/net/polybugger/apollot/db/DateTimeFormat.java
+++ app/src/main/java/net/polybugger/apollot/db/DateTimeFormat.java
@@ -3,15 +3,15 @@ package net.polybugger.apollot.db;
 public class DateTimeFormat {
 
     public static final String DATE_TIME_DB_TEMPLATE = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
-    public static final String DATE_TIME_DISPLAY_TEMPLATE = "MMM d, yyyy hh:mm aa";
-    public static final String DATE_TIME_DISPLAY_TEMPLATE_JA = "MMM d, yyyy aa hh:mm";
+    public static final String DATE_TIME_DISPLAY_TEMPLATE = "MMM d, yyyy h:mmaa";
+    public static final String DATE_TIME_DISPLAY_TEMPLATE_JA = "y'年'M'月'd'日' aah:mm";
 
     public static final String DATE_DB_TEMPLATE = "yyyy-MM-dd";
     public static final String DATE_DISPLAY_TEMPLATE = "MMM d, yyyy";
-    public static final String DATE_DISPLAY_TEMPLATE_JA = "MMM d, yyyy";
+    public static final String DATE_DISPLAY_TEMPLATE_JA = "y'年'M'月'd'日'";
 
     public static final String TIME_DB_TEMPLATE = "HH:mm:ss.SSSZ";
-    public static final String TIME_DISPLAY_TEMPLATE = "hh:mm aa";
-    public static final String TIME_DISPLAY_TEMPLATE_JA = "aa hh:mm";
+    public static final String TIME_DISPLAY_TEMPLATE = "h:mmaa";
+    public static final String TIME_DISPLAY_TEMPLATE_JA = "aah:mm";
 
 }
