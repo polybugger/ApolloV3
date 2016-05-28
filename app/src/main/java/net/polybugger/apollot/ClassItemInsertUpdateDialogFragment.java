ada14923fb94848f5cf699bc68b4f46133811227
Poly Bug <polybugger@gmail.com>
2016-05-28 20:05:04 +0800

    added functionality to hide perfect score edit text and submission due date button

--- app/src/main/java/net/polybugger/apollot/ClassItemInsertUpdateDialogFragment.java
+++ app/src/main/java/net/polybugger/apollot/ClassItemInsertUpdateDialogFragment.java
@@ -19,6 +19,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -49,11 +51,20 @@ public class ClassItemInsertUpdateDialogFragment extends AppCompatDialogFragment
 
     private Listener mListener;
     private ClassItemContract.ClassItemEntry mEntry;
+    private AlertDialog mAlertDialog;
     private LinearLayout mBackgroundLayout;
     private Button mItemDateButton;
+    private Spinner mItemTypeSpinner;
     private EditText mDescriptionEditText;
     private TextView mErrorTextView;
-    private Spinner mItemTypeSpinner;
+    private CheckBox mCheckAttendanceCheckBox;
+    private CheckBox mRecordScoresCheckBox;
+    private EditText mPerfectScoreEditText;
+    private TextView mPerfectScoreErrorTextView;
+    private CheckBox mRecordSubmissionsCheckBox;
+    private Button mSubmissionDueDateButton;
+    private TextView mSubmissionDueDateErrorTextView;
+
 
     public static ClassItemInsertUpdateDialogFragment newInstance(ClassItemContract.ClassItemEntry entry, String title, String buttonText, String fragmentTag) {
         ClassItemInsertUpdateDialogFragment df = new ClassItemInsertUpdateDialogFragment();
@@ -79,6 +90,13 @@ public class ClassItemInsertUpdateDialogFragment extends AppCompatDialogFragment
         mItemTypeSpinner = (Spinner) view.findViewById(R.id.item_type_spinner);
         mDescriptionEditText = (EditText) view.findViewById(R.id.description_edit_text);
         mErrorTextView = (TextView) view.findViewById(R.id.error_text_view);
+        mCheckAttendanceCheckBox = (CheckBox) view.findViewById(R.id.check_attendance_check_box);
+        mRecordScoresCheckBox = (CheckBox) view.findViewById(R.id.record_scores_check_box);
+        mPerfectScoreEditText = (EditText) view.findViewById(R.id.perfect_score_edit_text);
+        mPerfectScoreErrorTextView = (TextView) view.findViewById(R.id.perfect_score_error_text_view);
+        mRecordSubmissionsCheckBox = (CheckBox) view.findViewById(R.id.record_submissions_check_box);
+        mSubmissionDueDateButton = (Button) view.findViewById(R.id.submission_due_date_button);
+        mSubmissionDueDateErrorTextView = (TextView) view.findViewById(R.id.submission_due_date_error_text_view);
         mItemDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,7 +108,6 @@ public class ClassItemInsertUpdateDialogFragment extends AppCompatDialogFragment
                 }
             }
         });
-
         mItemTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             // TODO make sure the colors are preserved on theme change from system
             @Override
@@ -145,6 +162,57 @@ public class ClassItemInsertUpdateDialogFragment extends AppCompatDialogFragment
                 mErrorTextView.setText(" ");
             }
         });
+        mRecordScoresCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    mPerfectScoreEditText.setVisibility(View.VISIBLE);
+                    mPerfectScoreEditText.requestFocus();
+                    mPerfectScoreErrorTextView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    mPerfectScoreEditText.setVisibility(View.GONE);
+                    mPerfectScoreErrorTextView.setVisibility(View.GONE);
+                    mPerfectScoreErrorTextView.setText(" ");
+                }
+            }
+        });
+        mPerfectScoreEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+            @Override
+            public void afterTextChanged(Editable s) {
+                mPerfectScoreErrorTextView.setText(" ");
+            }
+        });
+        mRecordSubmissionsCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    mSubmissionDueDateButton.setVisibility(View.VISIBLE);
+                    mSubmissionDueDateButton.requestFocus();
+                    mSubmissionDueDateErrorTextView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    mSubmissionDueDateButton.setVisibility(View.GONE);
+                    mSubmissionDueDateErrorTextView.setVisibility(View.GONE);
+                    mSubmissionDueDateErrorTextView.setText(" ");
+                }
+            }
+        });
+        mSubmissionDueDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                DatePickerDialogFragment df = (DatePickerDialogFragment) fm.findFragmentByTag(DatePickerDialogFragment.TAG);
+                if(df == null) {
+                    df = DatePickerDialogFragment.newInstance((Date) v.getTag(), getString(R.string.submission_due_date_hint), getTag(), R.id.submission_due_date_button);
+                    df.show(fm, DatePickerDialogFragment.TAG);
+                }
+            }
+        });
 
         Context context = getContext();
         final SimpleDateFormat sdf;
@@ -165,17 +233,28 @@ public class ClassItemInsertUpdateDialogFragment extends AppCompatDialogFragment
         if(itemType != null)
             mItemTypeSpinner.setSelection(spinnerAdapter.getPosition(itemType));
         mDescriptionEditText.setText(mEntry.getDescription());
+        mCheckAttendanceCheckBox.setChecked(mEntry.isCheckAttendance());
+        mRecordScoresCheckBox.setChecked(mEntry.isRecordScores());
+        Float perfectScore = mEntry.getPerfectScore();
+        if(perfectScore != null)
+            mPerfectScoreEditText.setText(String.format("%.2f", perfectScore));
+        mRecordSubmissionsCheckBox.setChecked(mEntry.isRecordSubmissions());
+        Date submissionDueDate = mEntry.getSubmissionDueDate();
+        if(submissionDueDate != null) {
+            mSubmissionDueDateButton.setText(sdf.format(submissionDueDate));
+            mSubmissionDueDateButton.setTag(submissionDueDate);
+        }
 
-        final AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+        mAlertDialog = new AlertDialog.Builder(getActivity())
                 .setTitle(args.getString(TITLE_ARG))
                 .setView(view)
                 .setNegativeButton(R.string.cancel, null)
                 .setPositiveButton(args.getString(BUTTON_TEXT_ARG), null)
                 .create();
-        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+        mAlertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
             @Override
             public void onShow(DialogInterface dialog) {
-                alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                mAlertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         String description = mDescriptionEditText.getText().toString();
@@ -185,18 +264,44 @@ public class ClassItemInsertUpdateDialogFragment extends AppCompatDialogFragment
                             return;
                         }
                         mEntry.setDescription(description);
+                        mEntry.setItemDate((Date) mItemDateButton.getTag());
                         ClassItemTypeContract.ClassItemTypeEntry itemType = (ClassItemTypeContract.ClassItemTypeEntry) mItemTypeSpinner.getSelectedItem();
                         if(itemType != null && itemType.getId() != -1)
                             mEntry.setItemType(itemType);
                         else
                             mEntry.setItemType(null);
+                        mEntry.setCheckAttendance(mCheckAttendanceCheckBox.isChecked());
+                        boolean recordScores = mRecordScoresCheckBox.isChecked();
+                        Float perfectScore;
+                        try {
+                            perfectScore = Float.parseFloat(mPerfectScoreEditText.getText().toString());
+                        }
+                        catch(Exception e) {
+                            perfectScore = null;
+                        }
+                        if(recordScores && perfectScore == null) {
+                            mPerfectScoreErrorTextView.setText(R.string.please_enter_a_perfect_score);
+                            mPerfectScoreEditText.requestFocus();
+                            return;
+                        }
+                        mEntry.setRecordScores(recordScores);
+                        mEntry.setPerfectScore(perfectScore);
+                        boolean recordSubmissions = mRecordSubmissionsCheckBox.isChecked();
+                        Date submissionDueDate = (Date) mSubmissionDueDateButton.getTag();
+                        if(recordSubmissions && submissionDueDate == null) {
+                            mSubmissionDueDateErrorTextView.setText(R.string.please_input_a_submission_due_date);
+                            mSubmissionDueDateButton.requestFocus();
+                            return;
+                        }
+                        mEntry.setRecordSubmissions(recordSubmissions);
+                        mEntry.setSubmissionDueDate(submissionDueDate);
                         mListener.onConfirmInsertUpdateClassItem(mEntry, fragmentTag);
                         dismiss();
                     }
                 });
             }
         });
-        return alertDialog;
+        return mAlertDialog;
     }
 
     @Override
@@ -226,10 +331,16 @@ public class ClassItemInsertUpdateDialogFragment extends AppCompatDialogFragment
             sdf = new SimpleDateFormat(DateTimeFormat.DATE_DISPLAY_TEMPLATE_JA, context.getResources().getConfiguration().locale);
         else
             sdf = new SimpleDateFormat(DateTimeFormat.DATE_DISPLAY_TEMPLATE, context.getResources().getConfiguration().locale);
-        if(date != null)
-            mItemDateButton.setText(sdf.format(date));
-        else
-            mItemDateButton.setText(null);
-        mItemDateButton.setTag(date);
+
+        Button b = (Button) mAlertDialog.findViewById(buttonId);
+        if(date != null) {
+            b.setText(sdf.format(date));
+            if(buttonId == R.id.submission_due_date_button)
+                mSubmissionDueDateErrorTextView.setText(" ");
+        }
+        else {
+            b.setText(null);
+        }
+        b.setTag(date);
     }
 }
